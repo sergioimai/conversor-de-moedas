d3c2beb9f67cc6f14c5b7cb8b996d3b4bde4d492
Sergio Imai <104179443+sergioimai@users.noreply.github.com>
2022-09-15 14:57:42 -0300

    alteraçao de nomes, inclusao de novos parametros.

--- Conversor de Moedas/src/ConverterMoedas/ConverterDistancias.java
+++ Conversor de Moedas/src/ConverterMoedas/ConverterDistancias.java
@@ -10,7 +10,10 @@ public class ConverterDistancias {
 		String opcao = (JOptionPane.showInputDialog(null, "Escolha uma opção:", "Distância", JOptionPane.PLAIN_MESSAGE, null, new Object[] {
 				"Metros a Quilômetros",
 				"Metros a Centímetros",
-				"Metros a Milímetros"},"Escolha")).toString();
+				"Metros a Milímetros",
+				"Quilômetros a Metros",
+				"Centímetros a Metros",
+				"Milímetros a Metros"},"Escolha")).toString();
 		
 		switch (opcao) {
 			case "Metros a Quilômetros":
@@ -21,6 +24,15 @@ public class ConverterDistancias {
 				break;
 			case "Metros a Milímetros":
 				distancia.converterMetrosParaMilimetros (valorRecebido);
+				break;
+			case "Quilômetros a Metros":
+				distancia.converterQuilometrosParaMetros(valorRecebido);
+				break;
+			case "Centímetros a Metros":
+				distancia.converterCentimetrosParaMetros(valorRecebido);
+				break;
+			case "Milímetros a Metros":
+				distancia.converterMilimetrosParaMetros(valorRecebido);
 				break;
 			
 		}
