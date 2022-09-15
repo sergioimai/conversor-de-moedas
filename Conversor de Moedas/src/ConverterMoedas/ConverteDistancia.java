927eac1c8a132bf6ff0c6f5f1c866bfa7dfde569
Sergio Imai <104179443+sergioimai@users.noreply.github.com>
2022-09-15 14:48:12 -0300

    inclusão de parâmetros e ajuste pontual.

--- Conversor de Moedas/src/ConverterMoedas/ConverteDistancia.java
+++ Conversor de Moedas/src/ConverterMoedas/ConverteDistancia.java
@@ -6,21 +6,39 @@ public class ConverteDistancia {
 
 	public double converterMetrosParaQuilometros (double valorRecebido) {
 		double metrosParaQuilometros = valorRecebido / 1000;
-		JOptionPane.showInternalMessageDialog(null, valorRecebido + " Metros são " + metrosParaQuilometros + " Km");
+		JOptionPane.showInternalMessageDialog(null, valorRecebido + " Metros são " + metrosParaQuilometros + " Km.");
 		return metrosParaQuilometros;
 	}
 	
 	public double converterMetrosParaCentimetros (double valorRecebido) {
 		double metrosParaCentimetros = valorRecebido * 100;
 		metrosParaCentimetros = (double) Math.round(metrosParaCentimetros * 100d)/ 100;
-		JOptionPane.showInternalMessageDialog(null, valorRecebido + " Metros são " + metrosParaCentimetros + " Cm");
+		JOptionPane.showInternalMessageDialog(null, valorRecebido + " Metros são " + metrosParaCentimetros + " cm.");
 		return metrosParaCentimetros;
 	}
 	
 	public double converterMetrosParaMilimetros (double valorRecebido) {
 		double metrosParaMilimetros = valorRecebido * 1000;
 		metrosParaMilimetros = (double) Math.round(metrosParaMilimetros * 100d)/ 100;
-		JOptionPane.showInternalMessageDialog(null, valorRecebido + " Metros são " + metrosParaMilimetros + " Mm");
+		JOptionPane.showInternalMessageDialog(null, valorRecebido + " Metros são " + metrosParaMilimetros + " mm.");
 		return metrosParaMilimetros;
+	}
+	
+	public double converterQuilometrosParaMetros (double valorRecebido) {
+		double quilometrosParaMetros = valorRecebido * 1000;
+		JOptionPane.showInternalMessageDialog(null, valorRecebido + " Km são " + quilometrosParaMetros + " metros.");
+		return quilometrosParaMetros;
+	}
+	
+	public double converterCentimetrosParaMetros (double valorRecebido) {
+		double centimetrosParaMetros = valorRecebido / 100;
+		JOptionPane.showInternalMessageDialog(null, valorRecebido + " cm são " + centimetrosParaMetros + " metros.");
+		return centimetrosParaMetros;
+	}
+	
+	public double converterMilimetrosParaMetros (double valorRecebido) {
+		double milimetrosParametros = valorRecebido / 1000;
+		JOptionPane.showInternalMessageDialog(null, valorRecebido + " mm são " + milimetrosParametros + " metros.");
+		return milimetrosParametros;
 	}
 }
